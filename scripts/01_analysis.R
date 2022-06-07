ccdced967292d79ee369f52952e09245189b4ef8
kparrish92 <60477545+kparrish92@users.noreply.github.com>
2022-06-06 21:13:44 -0400

    initial commit

---
+++ scripts/01_analysis.R
@@ -0,0 +1,352 @@
+
+library(here)
+library(tidyverse)
+library(brms)
+library(bayesplot)
+library(bayestestR)
+
+
+df = read.csv(here("data", "tidy_data.csv")) %>% 
+  janitor::clean_names() %>% 
+  filter(!is.na(duration))
+
+# Descriptive statistics
+
+## Mean duration at each timepoint 
+
+df %>% 
+  group_by(session, group)  %>% 
+  summarize(mean_duration = mean(duration), sd_duration = sd(duration)) %>% 
+  ggplot(aes(y = mean_duration, x = session, color = as.factor(group))) + geom_point()
+
+df %>% 
+  group_by(session, group)  %>% 
+  summarize(mean_duration = mean(duration), sd_duration = sd(duration)) %>% 
+  ggplot(aes(y = mean_duration, x = as.factor(session), color = as.factor(group))) + 
+  geom_point() + geom_smooth()
+
+## Mean RI at each timepoint 
+
+df %>% 
+  group_by(session, group)  %>% 
+  summarize(mean_duration = mean(ri), sd_duration = sd(ri)) %>% 
+  ggplot(aes(y = mean_duration, x = session, color = as.factor(group))) + geom_point()
+
+## Number of type of realizations by timepoint 
+
+df %>% 
+  group_by(session, group, newest) %>% 
+  summarize(n = n()) %>% 
+  ggplot(aes(y = n, x = newest, fill = newest)) + geom_col(color = "black") + 
+  scale_x_discrete(limits=c("tap", "stop", "fricative")) +
+  theme(panel.background = element_rect(fill = "white")) +
+  facet_grid(group~session) +
+  theme(text=
+          element_text(
+            size=10,
+            family="Times New Roman")) +
+  theme(legend.text=element_text(size=8)) +
+  theme(legend.position = "bottom")
+
+# Group 1 individual 
+df %>% 
+  filter(group == "1") %>% 
+  group_by(session, partic, newest) %>% 
+  summarize(n = n()) %>% 
+  ggplot(aes(y = n, x = newest, fill = newest)) + geom_col(color = "black") + 
+  scale_x_discrete(limits=c("tap", "stop", "fricative")) +
+  theme(panel.background = element_rect(fill = "white")) +
+  facet_grid(partic~session) +
+  theme(text=
+          element_text(
+            size=10,
+            family="Times New Roman")) +
+  theme(legend.text=element_text(size=8)) +
+  theme(legend.position = "bottom")
+
+# Group 2 individual 
+df %>% 
+  filter(group == "2") %>% 
+  group_by(session, partic, newest) %>% 
+  summarize(n = n()) %>% 
+  ggplot(aes(y = n, x = newest, fill = newest)) + geom_col(color = "black") + 
+  scale_x_discrete(limits=c("tap", "stop", "fricative")) +
+  theme(panel.background = element_rect(fill = "white")) +
+  facet_grid(partic~session) +
+  theme(text=
+          element_text(
+            size=10,
+            family="Times New Roman")) +
+  theme(legend.text=element_text(size=8)) +
+  theme(legend.position = "bottom")
+
+
+## Group 3 Individual 
+
+df %>% 
+  filter(group == "3") %>% 
+  group_by(session, partic, newest) %>% 
+  summarize(n = n()) %>% 
+  ggplot(aes(y = n, x = newest, fill = newest)) + geom_col(color = "black") + 
+  scale_x_discrete(limits=c("tap", "stop", "fricative")) +
+  theme(panel.background = element_rect(fill = "white")) +
+  facet_grid(partic~session) +
+  theme(text=
+          element_text(
+            size=10,
+            family="Times New Roman")) +
+  theme(legend.text=element_text(size=8)) +
+  theme(legend.position = "bottom")
+
+
+
+df %>% 
+  filter(group == "3") %>% 
+  group_by(session, partic, newest) %>% 
+  summarize(n = n()) %>% 
+  ggplot(aes(y = n, x = as.factor(session), fill = newest, group = partic)) +
+  geom_point() + geom_line()
+  theme(panel.background = element_rect(fill = "white")) +
+  facet_grid(partic~session) +
+  theme(text=
+          element_text(
+            size=10,
+            family="Times New Roman")) +
+  theme(legend.text=element_text(size=8)) +
+  theme(legend.position = "bottom")
+### Model 
+
+df$group = as.factor(df$group)
+
+df$session = as.factor(df$session)
+
+mod1 <- 
+  brm(formula = newest ~ session + 
+        (session | partic) + (1 | token), 
+      warmup = 1000, iter = 2000, chains = 4, 
+      family = categorical(link = "logit"), 
+      cores = parallel::detectCores(), 
+      control = list(adapt_delta = 0.99, max_treedepth = 15), 
+      data = df %>% filter(group == 1),
+      file = here("data", "models", "mod1.rds"))
+
+
+
+summary(mod1)
+
+conditional_effects(mod1, categorical = TRUE)
+
+f = conditional_effects(mod1, categorical = TRUE)
+
+eff_df = f[["group:cats__"]]
+
+emmeans(mod1)
+
+
+mod2 <- 
+  brm(formula = newest ~ session + 
+        (session | partic) + (1 | token), 
+      warmup = 1000, iter = 2000, chains = 4, 
+      family = categorical(link = "logit"), 
+      cores = parallel::detectCores(), 
+      control = list(adapt_delta = 0.99, max_treedepth = 15), 
+      data = df %>% filter(group == 2),
+      file = here("data", "models", "mod2.rds"))
+
+
+
+mod3 <- 
+  brm(formula = newest ~ session + 
+        (session | partic) + (1 | token), 
+      warmup = 1000, iter = 2000, chains = 4, 
+      family = categorical(link = "logit"), 
+      cores = parallel::detectCores(), 
+      control = list(adapt_delta = 0.99, max_treedepth = 15), 
+      data = df %>% filter(group == 3),
+      file = here("data", "models", "mod3.rds"))
+
+
+conditional_effects(mod3, "session", resp = "fricative", categorical = TRUE)
+
+conditional_effects(mod3, categorical = TRUE)
+
+
+df$newest = as.factor(df$newest)
+
+df$session = as.factor(df$session)
+
+df$group = as.factor(df$group)
+df$newest = relevel(df$newest, ref = "tap")
+
+
+mod_int <- 
+  brm(formula = newest ~ session*group + 
+        (session | partic) + (1 | token), 
+      warmup = 1000, iter = 2000, chains = 4, 
+      family = categorical(link = "logit"), 
+      cores = parallel::detectCores(), 
+      control = list(adapt_delta = 0.99, max_treedepth = 15), 
+      data = df,
+      file = here("data", "models", "modint.rds"))
+
+
+mod_int_re <- 
+  brm(formula = newest ~ session*group + 
+        (session | partic) + (1 | token), 
+      warmup = 1000, iter = 2000, chains = 4, 
+      family = categorical(link = "logit"), 
+      cores = parallel::detectCores(), 
+      control = list(adapt_delta = 0.99, max_treedepth = 15), 
+      data = df,
+      file = here("data", "models", "modint_re.rds"))
+
+summary(mod_int_re)
+
+# Group 1 
+plogis(-5.83) # Session 1 
+plogis(-5.83 + 2.92) # Session 2
+plogis(-5.83 + 1.91) # Session 3
+
+# Group 2 
+plogis(-5.83 + 2.81) # Session 1
+plogis(-5.83 + 2.81 + .58) # Session 2 
+plogis(-5.83 + 2.81 + .55) # Session 3 
+
+# Group 3 
+plogis(-5.83 + 3.06) # Session 1
+plogis(-5.83 + 3.06 - .46) # Session 2 
+plogis(-5.83 + 3.06 - .35) # Session 3 
+
+
+conditions = make_conditions(mod_int_re, vars = "group")
+x = conditional_effects(mod_int_re, categorical = TRUE, conditions = conditions)
+
+
+
+eff_df = x[["session:cats__"]]
+
+
+eff_df$effect1__ <- factor(eff_df$effect1__, 
+                            levels = 
+                              c("6", 
+                                "5", 
+                                "1"))
+
+
+eff_df %>% 
+  ggplot(aes(x = estimate__, y = effect1__, fill = effect2__)) + 
+  geom_pointrange(aes(xmin = lower__, xmax = upper__), 
+                  shape = 21, 
+                  position = position_dodge(width = .5)) +
+  facet_grid(~group)
+
+
+eff_df$effect1__ <- factor(eff_df$effect1__, 
+                           levels = 
+                             c("1", 
+                               "5", 
+                               "6"))
+
+
+eff_df %>% 
+  filter(effect2__ == "fricative") %>% 
+  ggplot(aes(y = estimate__, x = effect1__, fill = group, group = group)) + 
+  geom_line(position = position_dodge(width = .5)) +
+  geom_pointrange(aes(ymin = lower__, ymax = upper__), shape = 21, 
+                  position = position_dodge(width = .5)) +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(
+          size = 0.1, 
+          linetype = 'solid',
+          colour = "grey")) +
+  ylab("Probability") + xlab("Test Time")
+
+
+fixef_df = fixef(mod_int) %>% 
+  as.data.frame() %>% 
+  rownames_to_column("parameter") %>% 
+  mutate(parameter = paste0("b_", parameter))
+
+
+posterior <- as.data.frame(mod_int)
+
+pars = colnames(posterior[1:18])
+
+
+
+
+#### Random effects 
+
+ranef_df = ranef(mod_int_re) 
+
+ranef_part = ranef_df[["partic"]] %>% 
+  as.data.frame()
+
+
+
+conditions_r = make_conditions(mod_int, vars = "partic")
+
+ex_r = conditional_effects(mod_int, categorical = TRUE, conditions = conditions_r)
+
+
+
+df_binom = df %>% 
+  mutate(prob_fric = case_when(
+    newest == "fricative" ~ 1,
+    newest == "stop" ~ 0, 
+    newest == "tap" ~ 0
+  ))
+
+df_binom$newest = as.factor(df_binom$newest)
+
+df_binom$session = as.factor(df_binom$session)
+
+df_binom$group = as.factor(df_binom$group)
+df_binom$newest = relevel(df_binom$newest, ref = "tap")
+
+
+
+mod_binomial <- 
+  brm(formula = prob_fric ~ session*group + 
+        (session | partic) + (1 | token), 
+      warmup = 1000, iter = 2000, chains = 4, 
+      family = bernoulli(link = "logit"), 
+      cores = parallel::detectCores(), 
+      control = list(adapt_delta = 0.99, max_treedepth = 15), 
+      data = df_binom,
+      file = here("data", "models", "mod_binom.rds"))
+
+
+fixef(mod_binomial)
+
+ran = ranef(mod_binomial) 
+
+ran_df = ran[["partic"]] %>% 
+  as.data.frame()
+
+plogis(ran_df$Estimate.Intercept)
+
+
+desc = df %>% 
+  group_by(partic, session, newest) %>% 
+  summarize(n = n()) %>% 
+  filter(newest == "fricative")
+
+conditional_effects(mod_binomial)
+
+fixef(mod_binomial)
+plogis(-6 + 3 -.74)
+
+ran_df = ran_df %>%
+  mutate(t = Estimate.session5 -.489)
+
+plogis(ran_df$t)
+
+fedf = fixef(mod_binomial) %>% 
+  as.data.frame()
+
+plogis(fedf$Estimate[1] + fedf$Estimate[2] + fedf$Estimate[4] + fedf$Estimate[6])
+
+plogis(fedf$Estimate[1] + fedf$Estimate[3] + fedf$Estimate[5] + fedf$Estimate[9])
+
+  
---
+++ scripts/plots.R
@@ -0,0 +1,301 @@
+
+df = read.csv(here("data", "tidy_data.csv")) %>% 
+  janitor::clean_names() %>% 
+  filter(!is.na(duration))
+
+
+df %>% 
+  group_by(session, group, newest) %>% 
+  summarize(n = n()) %>% 
+  ggplot(aes(y = n, x = newest, fill = newest)) + geom_col(color = "black") + 
+  scale_x_discrete(limits=c("tap", "stop", "fricative")) +
+  theme(panel.background = element_rect(fill = "white")) +
+  facet_grid(group~session) +
+  theme(text=
+          element_text(
+            size=10,
+            family="Times New Roman")) +
+  theme(legend.text=element_text(size=8)) +
+  theme(legend.position = "bottom") +
+  theme(legend.title = element_blank()) +
+  xlab("Type of /d/ realization") + 
+  ggsave(here("report", "figs", "desc_fig.png"), dpi = 300)
+
+
+mod_int_re = readRDS(here("models", "modint_re.rds"))
+posterior <- as.data.frame(mod_int_re)
+
+pars = colnames(posterior[1:18])
+
+
+fixef_df = fixef(mod_int_re) %>% 
+  as.data.frame() %>% 
+  rownames_to_column("parameter") %>% 
+  mutate(parameter = paste0("b_", parameter))
+
+
+mcmc_areas(posterior,
+           pars = pars,
+           prob = 0.8) + 
+  xlim(-20,40) + 
+  geom_text(data = mutate_if(fixef_df, is.numeric, round, 2),
+                                                  aes(label = paste(Estimate, "[",
+                                                                    Q2.5, "-", Q97.5,
+                                                                    "]"), x = Inf), 
+                                                  hjust = "inward", size = 3) + 
+  ggsave(here("report", "figs", "model_output.png"), dpi = 300)
+
+
+conditions = make_conditions(mod_int_re, vars = "group")
+x = conditional_effects(mod_int_re, categorical = TRUE, conditions = conditions)
+
+
+
+eff_df = x[["session:cats__"]]
+
+
+eff_df$effect1__ <- factor(eff_df$effect1__, 
+                           levels = 
+                             c("6", 
+                               "5", 
+                               "1"))
+
+
+eff_df %>% 
+  filter(effect2__ == "fricative") %>% 
+  ggplot(aes(y = estimate__, x = effect1__, fill = group, group = group)) + 
+  geom_line(position = position_dodge(width = .5)) +
+  geom_pointrange(aes(ymin = lower__, ymax = upper__), shape = 21, 
+                  position = position_dodge(width = .5)) +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(
+          size = 0.1, 
+          linetype = 'solid',
+          colour = "grey")) +
+  ylab("Probability") + xlab("Test Time") +
+  ggsave(here("report", "figs", "full_model.png"), dpi = 300)
+
+
+upper = eff_df %>% 
+  filter(effect2__ == "fricative") %>% 
+  select(group, effect1__, effect2__, upper__) %>% 
+  pivot_wider(names_from = effect1__, values_from = upper__) %>% 
+  select(group, `1`, `5`, `6`)
+
+lower = eff_df %>% 
+  filter(effect2__ == "fricative") %>% 
+  select(group, effect1__, effect2__, lower__) %>% 
+  pivot_wider(names_from = effect1__, values_from = lower__) %>% 
+  select(group, `1`, `5`, `6`)
+
+params = eff_df %>% 
+  filter(effect2__ == "fricative") %>% 
+  select(group, effect1__, effect2__, estimate__) %>% 
+  pivot_wider(names_from = effect1__, values_from = estimate__) %>% 
+  select(group, `1`, `5`, `6`) 
+
+params$hi_1 = upper$`1`
+params$hi_5 = upper$`5`
+params$hi_6 = upper$`6`
+
+params$lo_1 = lower$`1`
+params$lo_5 = lower$`5`
+params$lo_6 = lower$`6`
+
+param_table = params %>% 
+  mutate(`Session 1` = paste0(round(`1`, digits = 3), 
+                              " [", round(params$lo_1, digits = 3),"-", 
+                              round(params$hi_1, digits = 3), "]"),
+         `Session 5` = paste0(round(`5`, digits = 3), 
+                              " [", round(params$lo_5, digits = 3),"-", 
+                              round(params$hi_5, digits = 3), "]"),
+         `Session 6` = paste0(round(`6`, digits = 3), 
+                              " [", round(params$lo_6, digits = 3),"-", 
+                              round(params$hi_6, digits = 3), "]"))  %>% 
+  select(group, `Session 1`, `Session 5`, `Session 6`) %>% 
+  write.csv(here("report", "param.csv"))
+
+
+
+
+level_order = c(group_3, group_2, group_1)
+
+fricative_fixef = eff_df %>% 
+  filter(effect2__ == "fricative") %>% 
+  mutate(log_estimate = logit(estimate__)) %>% 
+  mutate(row = c(1:9))
+
+# Get participant lists 
+group_1_df = df %>% filter(group == "1")
+group_1 = unique(group_1_df$partic)
+
+
+group_2_df = df %>% filter(group == "2")
+group_2 = unique(group_2_df$partic)
+
+
+group_3_df = df %>% filter(group == "3")
+group_3 = unique(group_3_df$partic)
+
+
+### Mutate group info to ranef df 
+
+ranef_df = ranef(mod_int_re) 
+
+ranef_part = ranef_df[["partic"]] %>% 
+  as.data.frame()
+
+ranef_tidy = ranef_part %>% 
+  rownames_to_column("partic") %>% 
+  select(1:13) %>% 
+  mutate(group = case_when(partic =
+                             partic %in% group_1 ~ "1",
+                           partic %in% group_2 ~ "2",
+                           partic %in% group_3 ~ "3")) %>% 
+  mutate(session_1 = case_when(
+    group == "1" ~ fricative_fixef$log_estimate[1],
+    group == "2" ~ fricative_fixef$log_estimate[4],
+    group == "3" ~ fricative_fixef$log_estimate[7]
+  )) %>% 
+  mutate(session_5 = case_when(
+    group == "1" ~ fricative_fixef$log_estimate[2],
+    group == "2" ~ fricative_fixef$log_estimate[5],
+    group == "3" ~ fricative_fixef$log_estimate[8]
+  )) %>% 
+  mutate(session_6 = case_when(
+    group == "1" ~ fricative_fixef$log_estimate[3],
+    group == "2" ~ fricative_fixef$log_estimate[6],
+    group == "3" ~ fricative_fixef$log_estimate[9]
+  )) %>% 
+  mutate(re_session_1 = plogis(session_1 + Estimate.mufricative_Intercept),
+         re_session_1_lo = plogis(session_1 + Q2.5.mufricative_Intercept),
+         re_session_1_hi = plogis(session_1 + Q97.5.mufricative_Intercept),
+         re_session_5 = plogis(session_5 + Estimate.mufricative_session5),
+         re_session_5_lo = plogis(session_5 + Q2.5.mufricative_session5),
+         re_session_5_hi = plogis(session_5 + Q97.5.mufricative_session5),
+         re_session_6 = plogis(session_6 + Estimate.mufricative_session6),      
+         re_session_6_lo = plogis(session_6 + Q2.5.mufricative_session5),
+         re_session_6_hi = plogis(session_6 + Q97.5.mufricative_session6))
+
+
+ranef_tidy %>% 
+  ggplot(aes(x = re_session_1, y = factor(partic, level = level_order), 
+             fill = group)) +
+  geom_pointrange(aes(xmin = re_session_1_lo, xmax = re_session_1_hi), 
+                  shape = 21) +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(
+          size = 0.1, 
+          linetype = 'solid',
+          colour = "grey")) +
+  ylab("Participant") + xlab("Probability") + 
+  geom_text(data = mutate_if(ranef_tidy, is.numeric, round, 2),
+            aes(label = paste(re_session_1, "[",
+                              re_session_1_lo, "-", re_session_1_hi,
+                              "]"), x = Inf), 
+            hjust = "inward", size = 2.5) + xlim(0, 1.5) +
+  scale_x_continuous(breaks = seq(0, 1, .25), 
+                     limits = c(-.05,1.5), 
+                     expand = c(0,0)) + 
+  ggsave(here("report", "figs", "session1_ind.png"), dpi = 300)
+
+
+ranef_tidy %>% 
+  ggplot(aes(x = re_session_5, y = factor(partic, level = level_order), 
+             fill = group)) +
+  geom_pointrange(aes(xmin = re_session_5_lo, xmax = re_session_5_hi), 
+                  shape = 21) +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(
+          size = 0.1, 
+          linetype = 'solid',
+          colour = "grey")) +
+  ylab("Participant") + xlab("Probability") + 
+  geom_text(data = mutate_if(ranef_tidy, is.numeric, round, 2),
+            aes(label = paste(re_session_5, "[",
+                              re_session_5_lo, "-", re_session_5_hi,
+                              "]"), x = Inf), 
+            hjust = "inward", size = 2.5) + xlim(0, 1.5) +
+  scale_x_continuous(breaks = seq(0, 1, .25), 
+                     limits = c(-.05,1.5), 
+                     expand = c(0,0)) + 
+  ggsave(here("report", "figs", "session5_ind.png"), dpi = 300)
+
+
+
+ranef_tidy %>% 
+  ggplot(aes(x = re_session_6, y = factor(partic, level = level_order), 
+             fill = group)) +
+  geom_pointrange(aes(xmin = re_session_6_lo, xmax = re_session_6_hi), 
+                  shape = 21) +
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(
+          size = 0.1, 
+          linetype = 'solid',
+          colour = "grey")) +
+  ylab("Participant") + xlab("Probability") + 
+  geom_text(data = mutate_if(ranef_tidy, is.numeric, round, 2),
+            aes(label = paste(re_session_6, "[",
+                              re_session_6_lo, "-", re_session_6_hi,
+                              "]"), x = Inf), 
+            hjust = "inward", size = 2.5) +
+  scale_x_continuous(breaks = seq(0, 1, .25), 
+                     limits = c(-.05,1.5), 
+                     expand = c(0,0)) + 
+  ggsave(here("report", "figs", "session6_ind.png"), dpi = 300)
+
+
+
+### Change in prob from times 
+
+glimpse(ranef_tidy)
+ranef_tidy %>% 
+  select(partic, re_session_1, re_session_5, re_session_6) %>% 
+  mutate(change_1 = re_session_1 - re_session_5,
+         change_2 = re_session_5 - re_session_6) %>% 
+  select(partic, change_1, change_2) %>% 
+  pivot_longer(cols = c(2:3), names_to = "change_no", values_to = "value") %>%
+  mutate(group = case_when(partic =
+                             partic %in% group_1 ~ "1",
+                           partic %in% group_2 ~ "2",
+                           partic %in% group_3 ~ "3")) %>% 
+  ggplot(aes(y = value, x = change_no, color = partic, group = partic)) + geom_point() + 
+  geom_line() + geom_hline(yintercept = 0, linetype = "dashed") + facet_wrap(~group)
+   
+
+
+ranef_tidy %>% 
+  select(partic, re_session_1, re_session_5, re_session_6) %>% 
+  pivot_longer(cols = c(2:4), names_to = "session", values_to = "probability") %>%
+  mutate(group = case_when(partic =
+                             partic %in% group_1 ~ "1",
+                           partic %in% group_2 ~ "2",
+                           partic %in% group_3 ~ "3")) %>% 
+  ggplot(aes(y = probability, x = session, color = partic, group = partic)) + 
+  geom_point() + geom_line() + facet_wrap(~group)
+
+ranef_tidy %>% 
+  mutate(is_pos = re_session_1 - re_session_5) %>% 
+  filter(is_pos > 0) %>% 
+  select(partic, re_session_1, re_session_5, re_session_6) %>% 
+  pivot_longer(cols = c(2:4), names_to = "session", values_to = "probability") %>%
+  mutate(group = case_when(partic =
+                             partic %in% group_1 ~ "1",
+                           partic %in% group_2 ~ "2",
+                           partic %in% group_3 ~ "3")) %>% 
+  ggplot(aes(y = probability, x = session, color = partic, group = partic)) + 
+  geom_point() + geom_line() + facet_wrap(~group)
+
+
+  mutate(change_1 = re_session_1 - re_session_5,
+         change_2 = re_session_5 - re_session_6) %>% 
+  select(partic, change_1, change_2) %>% 
+  pivot_longer(cols = c(2:3), names_to = "change_no", values_to = "value") %>%
+  mutate(group = case_when(partic =
+                             partic %in% group_1 ~ "1",
+                           partic %in% group_2 ~ "2",
+                           partic %in% group_3 ~ "3")) %>% 
+  ggplot(aes(y = value, x = change_no, color = partic, group = partic)) + geom_point() + 
+  geom_line() + geom_hline(yintercept = 0, linetype = "dashed") + facet_wrap(~group)
+
+ 
