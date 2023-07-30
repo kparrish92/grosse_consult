7439f63d6dd9d9a1c6eb513b915762fdb5770a26
kparrish92 <kcp74@scarletmail.rutgers.edu>
2023-07-30 10:25:40 -0400

    add plots

--- scripts_new/04_plots.R
+++ scripts_new/04_plots.R
@@ -46,3 +46,48 @@ params %>%
   write.csv(here("report", "param_updated.csv"))
 
 ### Random effect plots 
+
+library(tidybayes)
+library(modelr)
+
+es_plot_function = function(group_1, session_1, group_2, session_2)
+
+{
+ 
+library(tidybayes) 
+library(modelr)
+session_all_fix = session_all %>% 
+  filter(session != 2) %>% 
+  filter(group == "Comparison" | group == "Experimental" | group == "Control")
+
+c = session_all_fix %>%
+  data_grid(session, group) %>%
+  add_epred_draws(mod, dpar = TRUE, category = "cat_binom",
+                   re_formula = NA)
+
+control_6 = c %>% 
+  filter(session == session_1 & group == group_1)
+
+exp_6 = c %>% 
+  filter(session == session_2 & group == group_2)
+
+
+exp_6$es = exp_6$.epred - control_6$.epred
+
+exp_6 %>%
+  mutate(is_positve = case_when(
+    es > 0 ~ 1,
+    es < 0 ~ 0)) %>% 
+  ggplot(aes(x = es, fill = after_stat(x < 0))) + stat_halfeye() + 
+  theme_minimal() +
+  scale_fill_manual(values=c("#009E73", "#ff6242")) + theme(legend.position = "none") +
+  xlab("Difference in probability") + ylab("Density") +
+  geom_text(aes(x = median(exp_6$es), 
+                y = .5, 
+                label = round(sum(is_positve)/4000, digits = 4), size = 7,
+            family = "sans"))
+}
+
+
+es_plot_function(group_1 = "Experimental", session_1 = 1, 
+                 group_2 = "Experimental", session_2 = 6)
